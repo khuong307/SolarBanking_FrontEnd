ec76513a1ba7029cb0a536d8c15ff6fdd93321ce
ddk992001 <ddk992001@gmail.com>
2022-12-20 01:51:52 +0700

    Basic Login

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -2,15 +2,47 @@ import {useForm} from "react-hook-form";
 import {useState} from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import {Link} from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function LoginForm(){
+    const navigate = useNavigate();
     const { register, handleSubmit, formState: { errors }} = useForm();
     const [passwordShown, setPasswordShown] = useState(false);
     const [verified, setVerified] = useState(false);
+    const [loginFailed, setLoginFailed] = useState({
+        isSuccess: true,
+        message: ''
+    });
     const RECAPTCHA_SITE_KEY = import.meta.env.VITE_RECAPTCHA_SITE_KEY;
+    const ROLE = {
+        CUSTOMER: 'Customer',
+        EMPLOYEE: 'Employee',
+        ADMIN: 'Admin'
+    }
 
     const onSubmit = function(data) {
-        alert(JSON.stringify(data));
+        axios.post('http://localhost:3030/api/accounts/authentication', data)
+            .then((res) => {
+                localStorage.solarBanking_accessToken = res.data.access_token;
+                localStorage.solarBanking_refreshToken = res.data.refresh_token;
+                localStorage.solarBanking_userId = res.data.account.user_id;
+                localStorage.solarBanking_username = res.data.account.username;
+                localStorage.solarBanking_userRole = res.data.account.role;
+
+                if (res.data.account.role === ROLE.CUSTOMER)
+                    navigate('/customer');
+                else if (res.data.account.role === ROLE.EMPLOYEE)
+                    navigate('/employee');
+                else
+                    navigate('/admin');
+            })
+            .catch((err) => {
+                setLoginFailed({
+                    isSuccess: false,
+                    message: err.response.data.message
+                });
+            });
     }
 
     const captchaChanged = function() {
@@ -20,6 +52,13 @@ function LoginForm(){
     const togglePasswordVisibility = function() {
         setPasswordShown(!passwordShown);
     };
+
+    const changeLoginFailedToDefault = function() {
+        setLoginFailed({
+            isSuccess: true,
+            message: ''
+        });
+    }
 
     return (
         <div>
@@ -47,7 +86,7 @@ function LoginForm(){
                                 <form onSubmit={handleSubmit(onSubmit)}>
                                     <div className="mr-lg-5 form-group d-flex align-items-center align-content-center">
                                         <i className="fa fa-user mr-2 user-icon"></i>
-                                        <input className="form-control email-input" placeholder="example@gmail.com" type="email"
+                                        <input className="form-control username-input" placeholder="Enter username" type="text"
                                                {...register("username", {
                                                    required: true,
                                                })}
@@ -58,7 +97,7 @@ function LoginForm(){
                                     }
                                     <div className="mr-lg-5 form-group d-flex align-items-center align-content-center" style={{position: "relative"}}>
                                         <i className="fa fa-lock mr-2 password-icon"></i>
-                                        <input className="form-control password-input" type={passwordShown ? "text" : "password"}
+                                        <input className="form-control password-input" placeholder="Enter password" type={passwordShown ? "text" : "password"}
                                                {...register("password", {
                                                    required: true,
                                                })}
@@ -72,6 +111,8 @@ function LoginForm(){
                                     {errors?.password?.type === "required" &&
                                         <p className="error-input"><i className="fa fa-warning mr-2"></i>Password is required!</p>
                                     }
+                                    {!loginFailed.isSuccess &&
+                                        <ErrorMessage error={loginFailed.message} changeLoginFailedToDefault={changeLoginFailedToDefault} />}
                                     <div className="mr-lg-5 d-flex justify-content-between align-content-center align-items-center recaptcha-wrapper">
                                         <ReCAPTCHA className="recaptcha" sitekey={RECAPTCHA_SITE_KEY} onChange={captchaChanged} />
                                         <Link to="/a" className="forget_password_link">
@@ -92,4 +133,20 @@ function LoginForm(){
         </div>
     );
 }
-export default LoginForm
+
+function ErrorMessage({ error, changeLoginFailedToDefault }) {
+    const handleCloseClicked = function() {
+        changeLoginFailedToDefault();
+    }
+
+    return (
+        <div className="err-wrapper alert alert-warning alert-dismissible fade show" role="alert">
+            {error}
+            <button onClick={handleCloseClicked} type="button" className="close-btn" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    );
+}
+
+export default LoginForm;
