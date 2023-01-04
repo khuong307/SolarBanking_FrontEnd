024d03a6d22cdff6f82d694f8aced6a82b997c37
ddk992001 <ddk992001@gmail.com>
2023-01-04 19:27:50 +0700

    Fix Debt List UI

--- src/components/customer/debt/table_debt_list_other.jsx
+++ src/components/customer/debt/table_debt_list_other.jsx
@@ -10,9 +10,10 @@ import '/src/assets/css/datatable-extension.css'
 import '/src/assets/css/data-table.css'
 
 function TableDebtListOther(props){
+    const debtListOther = props.debtListOther;
+    const isSelf = props.isSelf;
     const userId = localStorage.solarBanking_userId;
     const [reasonCancel,setReasonCancel] = useState("");
-    const [debtListOther,setDebtListOther] = useState([]);
     const [showDeleteModal,setShowDeleteModal] = useState({
         isShow: false,
         debt_id: null
@@ -26,25 +27,21 @@ function TableDebtListOther(props){
             debt_id: null
         })
     }
-    const handleSubmitDeleteModal = ()=>{
-        axiosInstance.delete(`/debtList/cancelDebt/${showDeleteModal.debt_id}`,{
+    const handleSubmitDeleteModal = ()=> {
+        axiosInstance.delete(`/debtList/cancelDebt/${showDeleteModal.debt_id}`, {
             user_id: userId,
             debt_cancel_message: reasonCancel,
         })
-            .then((res)=>{
+            .then((res) => {
                 //setDebtListOther(debtListOther.filter(debt => debt.debt_id !== showDeleteModal.debt_id))
                 handleCloseDeleteModal();
             })
-            .catch((err)=>{
+            .catch((err) => {
                 console.log(err);
             })
     }
 
-    useEffect(function (){
-        setDebtListOther(props.debtListOther);
-    },[])
-
-    useEffect(function (){
+    if (!isSelf) {
         const buttonComponent = `
             <div class="d-flex">
                 <button class="btn btn-info btn-edit">
@@ -55,45 +52,44 @@ function TableDebtListOther(props){
                 </button>
             </div>
         `;
-        if (debtListOther.length !==0){
-            $("#paidDebtOther").DataTable().rows().remove().draw();
-            for (const c of debtListOther) {
-                const ans = [];
-                ans.push(c.id)
-                ans.push(c.debt_account_number)
-                ans.push(formatMoney(c.debt_amount) + " VND")
-                ans.push(formateDateTime(c.debt_created_at))
-                ans.push(c.debt_status)
-                ans.push(buttonComponent)
-                $("#paidDebtOther").DataTable().rows.add(ans).draw(false);
+        if (Array.isArray(debtListOther)) {
+            if (debtListOther.length > 0) {
+                $("#paidDebtOther").DataTable().rows().remove().draw();
+                debtListOther.forEach((debt, debtIdx) => {
+                    const ans = [];
+                    ans.push(debtIdx + 1)
+                    ans.push(debt.debt_account_number)
+                    ans.push(formatMoney(debt.debt_amount) + " VND")
+                    ans.push(formateDateTime(debt.debt_created_at))
+                    ans.push(debt.debt_status)
+                    ans.push(buttonComponent)
+                    $("#paidDebtOther").DataTable().row.add(ans).draw(false);
+                });
+                const deleteBtnArr = document.getElementsByClassName('btn-delete');
+                for (let i = 0; i < deleteBtnArr.length; i++)
+                    deleteBtnArr[i].addEventListener('click', function(e) {
+                        setShowDeleteModal({
+                            isShow: true,
+                            debt_id: debtListOther[i].debt_id
+                        });
+                    });
             }
         }
-        const deleteBtnArr = document.getElementsByClassName('btn-delete');
-        for (let i = 0; i < deleteBtnArr.length; i++)
-            deleteBtnArr[i].addEventListener('click', function(e) {
-                setShowDeleteModal({
-                    isShow: true,
-                    debt_id: debtListOther[i].debt_id
-                });
-            });
-    },[debtListOther]);
-
+    }
 
     return (
-        typeof props.debtListOther == "object" &&
         <div className="table-responsive">
             <table id="paidDebtOther" className="display">
                 <thead>
                 <tr>
-                    <th>#</th>
-                    <th>User Id</th>
-                    <th>Amount</th>
-                    <th>Create Date</th>
-                    <th>Status</th>
-                    <th></th>
+                    <th scope="col">#</th>
+                    <th scope="col">Account Number</th>
+                    <th scope="col">Amount</th>
+                    <th scope="col">Create Date</th>
+                    <th scope="col">Status</th>
+                    <th scope="col"></th>
                 </tr>
                 </thead>
-
                 <tbody>
                 </tbody>
             </table>
